8948531dbfbd849a97f3f8dabf2c6611a0b34263
Jyothis-Tency <jyothisgtency@gmail.com>
2026-05-18 15:31:18 +0530

    Trim trailing slashes from API base URL in api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_BASE = (
+  import.meta.env.VITE_API_URL || "http://localhost:4000"
+).replace(/\/+$/, "");
 
 const getToken = () => localStorage.getItem("mailstation_token");
 
